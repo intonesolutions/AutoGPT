25c190eecc634c83beed41d93bdfbe4abef1e1b6
Tamer <tshazli@intonesolutions.com>
2025-10-13 03:46:34 -0400

    fix 41

--- autogpt_platform/frontend/_build-eks.ps1
+++ autogpt_platform/frontend/_build-eks.ps1
@@ -3,6 +3,6 @@ rm -Force -Recurse -Confirm:$false node_modules
 rm -Force -Recurse -Confirm:$false .next
 
 cp -Force .env.dev .env.local
-npm install
+npm install --legacy-peer-deps
 # pnpm run generate:api:force
 npm build
